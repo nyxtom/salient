fd1124ec6d3bb2a1c24bf817fdad88f40a65f783
Thomas Holloway <nyxtom@gmail.com>
2013-10-22 17:37:54 -0600

    Adding basic numerics catch with decimals

--- spec/wordpunct_tokenizer_spec.js
+++ spec/wordpunct_tokenizer_spec.js
@@ -3,10 +3,16 @@ var Tokenizer = require('../lib/salient/tokenizers/wordpunct_tokenizer'),
 
 describe('wordpunct_tokenizer', function () {
 
-    it('should tokenize on words and punctuation', function () {
+    it('should tokenize on words and punctuation and money', function () {
         var cleanText = 'data, here to clean.Wouldn\'t you say so? I have $20.45, are you ok?';
         var tokens = tokenizer.tokenize(cleanText);
         expect(tokens).toEqual([ 'data', ',', 'here', 'to', 'clean', '.', 'Wouldn', '\'', 't', 'you', 'say', 'so', '? ', 'I', 'have', '$20.45', ',', 'are', 'you', 'ok', '?' ]);
+    });
+
+    it('should tokenize on words and punctuation and numerics', function () {
+        var cleanText = 'data, here to clean.Wouldn\'t you say so? I have 23.22! Are you ok?';
+        var tokens = tokenizer.tokenize(cleanText);
+        expect(tokens).toEqual([ 'data', ',', 'here', 'to', 'clean', '.', 'Wouldn', '\'', 't', 'you', 'say', 'so', '? ', 'I', 'have', '23.22', '!', 'Are', 'you', 'ok', '?' ]);
     });
 
 });
