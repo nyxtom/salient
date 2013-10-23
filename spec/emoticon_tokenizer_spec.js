74bfc4dcec9aa7eda972738a4fa0b0c3a4ab82fb
Thomas Holloway <nyxtom@gmail.com>
2013-10-23 10:03:07 -0600

    Adding test case for returning results for including indices

--- spec/emoticon_tokenizer_spec.js
+++ spec/emoticon_tokenizer_spec.js
@@ -10,4 +10,12 @@ describe('emoticon_tokenizer', function () {
         expect(tokenizer.tokenize(text)).toEqual([ '>:(' ]);
     });
 
+    it('should tokenize where possible on emoticons and return indices', function () {
+        var tokenizer = new Tokenizer({ includeIndices: true });
+        expect(tokenizer.tokenize("RT @sampleusername: this is typical :)"))
+            .toEqual([ { v : ':)', indices : [ 36, 38 ] } ]);
+        var text = tokenizer.clean("HTML entities & other Web oddities can be an &aacute;cute <em class='grumpy'>pain</em> >:(");
+        expect(tokenizer.tokenize(text)).toEqual([ { v : '>:(', indices : [ 87, 90 ] } ]);
+    });
+
 });
