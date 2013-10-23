9e61a1238ef4182c328fc3cad89f5c7090e1b605
Thomas Holloway <nyxtom@gmail.com>
2013-10-23 12:21:15 -0600

    Adding support for time matching and nominal numerics (1st, 2nd..etc)

--- spec/wordpunct_tokenizer_spec.js
+++ spec/wordpunct_tokenizer_spec.js
@@ -18,7 +18,13 @@ describe('wordpunct_tokenizer', function () {
     it('should tokenize on words and punctuation and many numerics', function () {
         var cleanText = 'data, here to clean.Wouldn\'t you say so? I have -23,323,234! Are you 2,234.00?';
         var tokens = tokenizer.tokenize(cleanText);
-        expect(tokens).toEqual([ 'data', ',', 'here', 'to', 'clean', '.', 'Wouldn', '\'', 't', 'you', 'say', 'so', '? ', 'I', 'have', '-23,323,234', '!', 'Are', 'you', '2,234.00', '?' ]);
+        expect(tokens).toEqual([ 'data', ',', 'here', 'to', 'clean', '.', 'Wouldn', '\'', 't', 'you', 'say', 'so', '? ', 'I', 'have', '-', '23,323,234', '!', 'Are', 'you', '2,234.00', '?' ]);
+    });
+
+    it('should tokenize on words and punctuation with times', function () {
+        var text = "From 12:00 am-11:59 pm. on Nov. 12th, you can make donation online to support Wylie Center.";
+        var tokens = tokenizer.tokenize(text);
+        expect(tokens).toEqual([ 'From', '12:00 am', '-', '11:59 pm.', 'on', 'Nov', '.', '12th', ',', 'you', 'can', 'make', 'donation', 'online', 'to', 'support', 'Wylie', 'Center', '.' ]);
     });
 
 });
