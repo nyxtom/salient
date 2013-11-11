232fc11c057f7e93ca50650fa9bf168c6bc7c80e
Thomas Holloway <nyxtom@gmail.com>
2013-11-11 11:46:40 -0700

    Adding multivariate linear regression algorithms with gradient descent implementation

---
+++ spec/regression/linear_regression_spec.js
@@ -0,0 +1,24 @@
+
+var salient = require('./../../');
+var LinearRegression = salient.regression.LinearRegression;
+
+describe('linear_regression', function () {
+
+    it('should be able to add examples and normalize them', function () {
+        var regression = new LinearRegression();
+        regression.addExample([1,2,3], 4);
+        regression.addExample([4,5,6], 4);
+        var normalizedMatrix = regression.normalize();
+        expect(normalizedMatrix.eql($M([[1,-1,-1,-1],[1,1,1,1]]))).toBeTruthy();
+    });
+
+    it('should be able to load training, normalize and normalize input', function () {
+        var regression = new LinearRegression();
+        regression.load('./spec/regression/ex1data2.txt');
+        regression.normalize();
+        var result = regression.train();
+        var normalInput = regression.normalizeInput([1650,3]);
+        expect(regression.calculate(result.theta, normalInput).e(1)).toEqual(293081.46436300856);
+    });
+
+});
