c4b5513db5934405214a77b420621aeabb67dc11
Thomas Holloway <nyxtom@gmail.com>
2014-01-15 18:20:01 -0700

    Adding brown corpus analyzer and updating tag mapping to universal tagset

---
+++ bin/browncorpus.js
@@ -0,0 +1,22 @@
+
+var path = require('path');
+var salient = require('./../');
+var BrownCorpus = salient.corpus.BrownCorpus;
+
+var args = process.argv;
+if (!args || args.length == 2) {
+    console.log('usage: node browncorpus.js ~/Public/brown [outputdir]');
+    return;
+}
+
+var file = args[2];
+var outputDir = __dirname;
+if (args.length > 3) {
+    if (args[3] == "~") {
+        outputDir = process.env['HOME'];
+    }
+}
+
+var brown = new BrownCorpus(file);
+brown.output = path.join(outputDir, 'en-brown.tag.dist');
+brown.parse();
