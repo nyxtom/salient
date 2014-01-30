3f163ea75d3dee0a690c5c62312c0368703d2f35
Thomas Holloway <nyxtom@gmail.com>
2014-01-29 22:25:43 -0700

    Updating hmmtest and hmm to use tag frequencies

--- bin/hmmtest.js
+++ bin/hmmtest.js
@@ -6,18 +6,14 @@ var model = new salient.language.HiddenMarkovModel();
 model.restore(path.join(__dirname, 'model.json'));
 model.restore(path.join(__dirname, 'model.tagdist.json'));
 
-var results = model.viterbi(['Where','is','the','train','?']);
-/*
-for (var k = 0; k < results.pi.length; k++) {
-    var item = results.pi[k];
-    var argMax;
-    for (var t1 in item) {
-        for (var t2 in item[t1]) {
-            var prob = item[t1][t2];
-            if (!argMax || prob > argMax.p) {
-                argMax = { t1: t1, t2: t2, p: prob };
-            }
-        }
-    }
-}*/
+var results = model.viterbi(['i','am','starting','to','have','faith']);
+console.log(JSON.stringify(results));
+
+results = model.viterbi(['this','is','another','test']);
+console.log(JSON.stringify(results));
+
+results = model.viterbi(['the','programmer','is','coding','every','day']);
+console.log(JSON.stringify(results));
+
+results = model.viterbi(['the','dog','is','chewing','on','my','leg',',','however','i','think','he','is','cute','and','i','really','like','him']);
 console.log(JSON.stringify(results));
