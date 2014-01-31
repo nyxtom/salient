8f5a40c7bc6a08e2646fc1d60012ed30e423ba6a
Thomas Holloway <nyxtom@gmail.com>
2014-01-30 17:20:35 -0700

    Updating hmmtags to allow an output directory

--- bin/hmmtags.js
+++ bin/hmmtags.js
@@ -7,16 +7,23 @@ var salient = require('./../');
 
 var args = process.argv;
 if (!args || args.length < 3) {
-    console.log('usage: node hmmtags en-brown.tag.dist');
+    console.log('usage: node hmmtags en-brown.tag.dist [outputdir]');
     return;
 }
 
 var model = new salient.language.HiddenMarkovModel();
 var tagDistFile = args[2];
+var outputDir = __dirname;
+if (args.length == 4) {
+    outputDir = args[3];
+    if (outputDir.indexOf('./') == 0) {
+        outputDir = path.join(__dirname, outputDir);
+    } 
+}
 
 // initialize a vocabulary with the size 12 tagsets
 model.loadTagDist(tagDistFile, function () {
     model.estimateTagDistribution();
     var s = JSON.stringify(model);
-    fs.writeFileSync(path.join(__dirname, 'model.tagdist.json'), s);
+    fs.writeFileSync(path.join(outputDir, 'model.tagdist.json'), s);
 });
