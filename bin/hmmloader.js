3edcece765336fcdb7a283e3e07f98bad67be942
Thomas Holloway <nyxtom@gmail.com>
2014-02-03 17:40:40 -0700

    Updating hmmload to automatically create new models

--- bin/hmmloader.js
+++ bin/hmmloader.js
@@ -1,7 +1,12 @@
 
+var fs = require('fs');
+var path = require('path');
 var salient = require('./../')
 var model = new salient.language.HiddenMarkovModel();
 model.restore(path.join(__dirname, 'model.json'));
 model.restore(path.join(__dirname, 'model.tagdist.json'));
+
+var json = JSON.stringify(model);
+fs.writeFileSync('en.hmm.json', json);
 
 module.exports = model;
