52755cf70c70c7773a84fad74daf871f9637169d
Thomas Holloway <nyxtom@gmail.com>
2014-02-05 17:53:33 -0700

    Adding glossary spec test

---
+++ spec/glossary/glossary_spec.js
@@ -0,0 +1,15 @@
+
+var salient = require('./../../');
+
+describe('glossary', function () {
+    it('should be able to parse a sentence', function () {
+        var glossary = new salient.glossary.Glossary();
+        glossary.parse('Some corn farmers are going against the grain by growing fruits and vegetables.');
+    });
+    it('should be able to parse and derive concepts', function () {
+        var glossary = new salient.glossary.Glossary();
+        glossary.parse('Some corn farmers are going against the grain by growing fruits and vegetables.');
+        var concepts = glossary.concepts();
+        expect(concepts).toEqual(['Some corn farmers', 'the grain', 'fruits', 'vegetables']);
+    });
+});
