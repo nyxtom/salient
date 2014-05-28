150241d7ebe3c59d09b8b9f40790f70d1b1cdeda
Thomas Holloway <nyxtom@gmail.com>
2014-05-28 14:40:16 -0600

    Adding wiktionary parseheadings file

---
+++ bin/parsewik-headings.js
@@ -0,0 +1,23 @@
+var salient = require('./../');
+var path = require('path');
+
+var args = process.argv;
+if (!args || args.length == 2) {
+    console.log('usage: node parsewik.js ~/enwiktionary-20140102-pages-articles.xml [outputdir]');
+    return;
+}
+
+var file = args[2];
+var outputDir = __dirname;
+if (args.length > 3) {
+    if (args[3] == "~") {
+        outputDir = process.env['HOME'];
+    }
+}
+
+// execute the parser with the given file
+var parser = new salient.wiktionary.WiktionaryParser(file);
+parser.printHeadings = true;
+parser.output = path.join(outputDir, parser.lang + '.wik.vocab');
+parser.outputDist = path.join(outputDir, parser.lang + '.wik.dist');
+parser.parse();
