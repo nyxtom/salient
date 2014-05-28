beb75a6d2e20f18e60489d2b518bf8b37489a600
Thomas Holloway <nyxtom@gmail.com>
2014-05-28 17:44:36 -0600

    Updating hmmloader to add command line arguments

--- bin/hmmloader.js
+++ bin/hmmloader.js
@@ -3,10 +3,27 @@ var fs = require('fs');
 var path = require('path');
 var salient = require('./../')
 var model = new salient.language.HiddenMarkovModel();
-model.restore(path.join(__dirname, 'model.json'));
-model.restore(path.join(__dirname, 'model.tagdist.json'));
+
+var args = process.argv;
+if (!args || args.length == 2) {
+    console.log('usage: node hmmloader.js model.json model.tagdist.json language.hmm.json');
+    return;
+}
+
+var modelVocabFile = 'model.json';
+var modelTagDistFile = 'model.tagdist.json';
+if (args && args.length > 2) {
+    modelVocabFile = args[2];
+    if (args.length > 3)
+        modelTagDistFile = args[3];
+    if (args.length > 4)
+        outputFile = args[4];
+}
+
+model.restore(path.join(__dirname, modelVocabFile));
+model.restore(path.join(__dirname, modelTagDistFile));
 
 var json = JSON.stringify(model);
-fs.writeFileSync('en.hmm.json', json);
+fs.writeFileSync(outputFile || 'output.hmm.json', json);
 
 module.exports = model;
