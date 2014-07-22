8e734e959fb21f1e72133f57bc79b5c3ba1a01fc
Thomas Holloway <nyxtom@gmail.com>
2014-07-22 16:43:32 -0600

    Adding simple rate limited content crawler using custom async module

--- bin/crawl.js
+++ bin/crawl.js
@@ -1,11 +1,14 @@
 
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path');
+
 var JSONStream = require('JSONStream');
 var salient = require('./../');
 
 var args = require('minimist')(process.argv);
-if (!args || !args.sitemaps) {
+if (!args.sitemaps && !args.content && args._.length < 2) {
     console.log('Usage: node crawl.js --sitemaps=true --concurrency=2 --output=tmp.txt http://www.example.com/sitemap.xml');
+    console.log('\t node crawl.js --content=true ./tmp.txt --concurrency=2')
     return;
 }
 
@@ -16,36 +19,56 @@ if (args.concurrency) {
     options.concurrency = args.concurrency;
 }
 
-var crawler = new salient.crawlers.SitemapCrawler(options);
-crawler.on('download-begin', function (queueItem) {
+var crawler = null;
+function downloadBegin(queueItem) {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
     process.stdout.write('Crawling ' + queueItem.loc + "...");
-});
-crawler.on('download-progress', function (queueItem, state) {
+};
+function downloadProgress(queueItem, state) {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
     process.stdout.write("Downloading " + queueItem.loc + " [" + state.received + " bytes]");
-});
-crawler.on('download-end', function (queueItem) {
+};
+function downloadEnd(queueItem) {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
-    process.stdout.write('Parsing ' + queueItem.loc + ' content...');
-});
+    process.stdout.write('Processing ' + queueItem.loc + ' content...');
+};
+
+
+if (args.content) {
+    crawler = new salient.crawlers.ContentCrawler(options);
+}
+else if (args.sitemaps) {
+    crawler = new salient.crawlers.SitemapCrawler(options);
+}
+
+crawler.on('download-begin', downloadBegin);
+crawler.on('download-progress', downloadProgress);
+crawler.on('download-end', downloadEnd);
 crawler.on('parse-end', function (queueItem) {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
     console.log('✓ Successfully processed ' + queueItem.loc + ' [' + queueItem.count + ' urls]');
 });
 crawler.on('end', function () {
-    console.log('Found ' + crawler.processed + ' total urls' + (args.output ? ' outputed to ' + args.output : ''));
+    if (crawler.processed) {
+        console.log('Found ' + crawler.processed + ' total urls' + (args.output ? ' outputed to ' + args.output : ''));
+    }
 });
 
 if (args.output) {
     var output = fs.createWriteStream(args.output);
     crawler.pipe(JSONStream.stringify(false)).pipe(output);
 }
-
-if (args.sitemaps) {
-    crawler.crawl(args._.slice(2));
+else if (args.contentdir) {
+    var index = 0;
+    crawler.on('data', function (data) {
+        fs.writeFile(path.join(args.contentdir, (index++) + ".txt"), data, function (err) {
+            if (err) console.log(err);
+        });
+    });
 }
+
+crawler.crawl(args._.slice(2));
