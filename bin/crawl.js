ce6f1c9794c8ae4f2e3556c6d5360158747e774c
Thomas Holloway <nyxtom@gmail.com>
2014-07-24 09:58:01 -0600

    Updating crawler to save state and emit url/data
    - Content crawler will now emit a data array of the [content-body, url]

--- bin/crawl.js
+++ bin/crawl.js
@@ -8,12 +8,25 @@ var salient = require('./../');
 var args = require('minimist')(process.argv);
 if (!args.sitemaps && !args.content && args._.length < 2) {
     console.log('Usage: node crawl.js --sitemaps=true --concurrency=2 --output=tmp.txt http://www.example.com/sitemap.xml');
-    console.log('\t node crawl.js --content=true ./tmp.txt --delayms=500')
+    console.log('\t node crawl.js --content=true ./tmp.txt --delayms=500');
+    console.log('\t node crawl.js --content=true --contentdir=./dump/ --delayms=500 --offset=1000 ./tmp.txt');
+    console.log('\t node crawl.js --state=true ./state.crawl');
     return;
 }
 
 
 // Setup additional command line options
+if (args.state) {
+    var file = fs.readFileSync(args._.slice(2)[0]);
+    var fileState = JSON.parse(file.toString());
+    if (fileState.args) {
+        args = fileState.args;
+        if (fileState.index) {
+            args.offset = fileState.index;
+        }
+    }
+}
+
 var options = {};
 if (args.concurrency) {
     options.concurrency = args.concurrency;
@@ -61,17 +74,55 @@ crawler.on('end', function () {
     }
 });
 
+var readOffset = 0;
+var index = args.offset || 0;
+var skip = index > 0;
+var readOffset = 0;
 if (args.output) {
     var output = fs.createWriteStream(args.output);
     crawler.pipe(JSONStream.stringify(false)).pipe(output);
 }
 else if (args.contentdir) {
-    var index = 0;
+    setInterval(function () {
+        saveState();
+    }, 5000);
+
     crawler.on('data', function (data) {
-        fs.writeFile(path.join(args.contentdir, (index++) + ".txt"), data, function (err) {
-            if (err) console.log(err);
-        });
+        if (skip && readOffset < index) {
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            process.stdout.write('Skipping lines ' + readOffset + '...');
+            readOffset++;
+        }
+        else {
+            if (skip) skip = false;
+            var loc = data[1];
+            loc = loc.substring(loc.lastIndexOf('/') + 1);
+            index++;
+            fs.writeFile(path.join(args.contentdir, loc), data[0], function (err) {
+                if (err) console.log(err);
+                data = null;
+            });
+        }
     });
 }
 
 crawler.crawl(args._.slice(2));
+
+function saveState(exitProc) {
+    if (args.contentdir) {
+        fs.writeFile(path.join(args.contentdir, '_state.crawl'), JSON.stringify({ 'args': args, 'index': index }), function (err) { 
+            if (exitProc) {
+                process.exit();
+            }
+        });
+    }
+}
+
+function exit() {
+    saveState(true);
+}
+
+process.on('SIGINT', exit);
+process.on('SIGTERM', exit);
+process.on('SIGUSR1', exit);
